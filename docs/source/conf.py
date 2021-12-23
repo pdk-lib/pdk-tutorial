649125b34cd2a50a39162ee688dd12d0b9fd23b8
Javad Roshdi <j.roshdi@gmail.com>
2021-12-23 14:37:11 +0330

    change configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PDK'
+copyright = '2021, PDK,Roshdi'
+author = 'Javad Roshdi'
 
 release = '0.1'
 version = '0.1.0'
